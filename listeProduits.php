4d9d48d9973ae4bc36db95a631768affff109740
Ayoub mchichi <a.mchichi@laposte.net>
2021-01-22 14:04:25 +0100

    fixed content duplicated when I run the query

--- listeProduits.php
+++ listeProduits.php
@@ -10,7 +10,6 @@ $pass = "";
 try {
     $db = new PDO("mysql:host=localhost;dbname=ecommerce;charset=utf8", "root", "");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "<p class='alert-success'>Connected</p>";
 } catch (PDOException $exception) {
     die("Erreur de connexion a PDO MySQL :" . $exception->getMessage());
 }
@@ -23,44 +22,45 @@ $sql = "SELECT * FROM produits";
 
 $req = $db->query($sql);
 
-foreach ($db->query("SELECT * FROM produits WHERE nom_produit = 'mx ergo'") as $title1);
-foreach ($db->query("SELECT * FROM produits WHERE nom_produit = 'MX MASTER 3'") as $title2);
-foreach ($db->query("SELECT * FROM produits WHERE nom_produit = 'G903'") as $title3);
-foreach ($db->query("SELECT * FROM produits") as $row); {
+foreach ($db->query("SELECT * FROM produits WHERE id_produit=1") as $row1);
+foreach ($db->query("SELECT * FROM produits WHERE id_produit=2") as $row2);
+foreach ($db->query("SELECT * FROM produits WHERE id_produit=3") as $row3); {
 
 ?>
     <div class="container text-center mt-5">
         <div class="row">
 
-            <div class="col-lg-4">
+            <div class="col-lg-4 d-flex align-items-stretch">
                 <div class="card mt-3">
-                    <img src="./img/m705-refresh-pdp.png" alt="" width="320">
-                    <h5 class="font-weight-bold mt-3"><?php echo $title1['nom_produit'] ?></h5>
-                    <p class="mt-3 lead"><?php echo $row['description_produit'] ?></p>
-                    <a href="" class="btn btn-warning p-3 m-2 font-weight-bold">En savoir plus</a>
-                    <a href="" class="btn btn-success p-3 m-2 font-weight-bold">Acheter maintenant</a>
-                    <a href="" class="btn btn-primary p-3 m-2 font-weight-bold">Ajouter au panier</a>
-                    <a href="" class="btn btn-danger p-3 m-2 font-weight-bold">Nous contacter</a>
-
-                </div>
-            </div>
-
-            <div class="col-lg-4">
-                <div class="card mt-3">
-                    <img src="./img/mx-master-2s.png" alt="" width="320">
-                    <h5 class="font-weight-bold mt-3"><?php echo $title2['nom_produit'] ?></h5>
-                    <p class="mt-3 lead"><?php echo $row['description_produit'] ?></p>
+                    <img src="<?php echo $row1['image_produit'] ?>" alt="<?php echo $row1['nom_produit'] ?>" title="<?php echo $row1['nom_produit'] ?>" width="320">
+                    <h5 class="font-weight-bold mt-3"><?php echo $row1['nom_produit'] ?></h5>
+                    <p class="mt-3 lead"><?php echo $row1['description_produit'] ?></p>
+                    <h5 class="font-weight-bold mt-3"><?php echo $row1['prix_produit'] ?> EUR</h5>
                     <a href="" class="btn btn-warning p-3 m-2 font-weight-bold">En savoir plus</a>
                     <a href="" class="btn btn-success p-3 m-2 font-weight-bold">Acheter maintenant</a>
                     <a href="" class="btn btn-primary p-3 m-2 font-weight-bold">Ajouter au panier</a>
                     <a href="" class="btn btn-danger p-3 m-2 font-weight-bold">Nous contacter</a>
                 </div>
             </div>
-            <div class="col-lg-4">
+
+            <div class="col-lg-4 d-flex align-items-stretch">
                 <div class="card mt-3">
-                    <img src="./img/g502-lightspeed-gallery-1.png" alt="" width="360">
-                    <h5 class="font-weight-bold mt-3"><?php echo $title3['nom_produit'] ?></h5>
-                    <p class="mt-3 lead"><?php echo $row['description_produit'] ?></p>
+                    <img src="<?php echo $row2['image_produit'] ?>" alt="<?php echo $row2['nom_produit'] ?>" title="<?php echo $row3['nom_produit'] ?>" width="320">
+                    <h5 class="font-weight-bold mt-3"><?php echo $row2['nom_produit'] ?></h5>
+                    <p class="mt-3 lead"><?php echo $row2['description_produit'] ?></p>
+                    <h5 class="font-weight-bold mt-3"><?php echo $row2['prix_produit'] ?> EUR</h5>
+                    <a href="" class="btn btn-warning p-3 m-2 font-weight-bold">En savoir plus</a>
+                    <a href="" class="btn btn-success p-3 m-2 font-weight-bold">Acheter maintenant</a>
+                    <a href="" class="btn btn-primary p-3 m-2 font-weight-bold">Ajouter au panier</a>
+                    <a href="" class="btn btn-danger p-3 m-2 font-weight-bold">Nous contacter</a>
+                </div>
+            </div>
+            <div class="col-lg-4 d-flex align-items-stretch">
+                <div class="card mt-3">
+                    <img src="<?php echo $row3['image_produit'] ?>" alt="<?php echo $row3['nom_produit'] ?>" title="<?php echo $row3['nom_produit'] ?>" width="360">
+                    <h5 class="font-weight-bold mt-3"><?php echo $row3['nom_produit'] ?></h5>
+                    <p class="mt-3 lead"><?php echo $row3['description_produit'] ?></p>
+                    <h5 class="font-weight-bold mt-3"><?php echo $row3['prix_produit'] ?> EUR</h5>
                     <a href="" class="btn btn-warning p-3 m-2 font-weight-bold">En savoir plus</a>
                     <a href="" class="btn btn-success p-3 m-2 font-weight-bold">Acheter maintenant</a>
                     <a href="" class="btn btn-primary p-3 m-2 font-weight-bold">Ajouter au panier</a>
@@ -76,6 +76,7 @@ foreach ($db->query("SELECT * FROM produits") as $row); {
 }
 
 ?>
+
 
 
 
